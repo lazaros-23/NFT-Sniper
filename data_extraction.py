4978fda621cb1943e33ef9a158182c01aed3e163
kiriakospapa <kyriakospapadopoulosauto@gmail.com>
2022-01-13 23:29:33 +0200

    The script is functionable

--- data_extraction.py
+++ data_extraction.py
@@ -19,10 +19,15 @@ tag =  soup.find_all('script', {'id' : '__NEXT_DATA__'})
 
 # Make some replacements that they might be needed
 tag = str(tag).replace('script id="__NEXT_DATA__" type="application/json">', '')
+tag = str(tag).replace('/script', '')
 tag = tag.replace('<', '')
-tag = str(tag).replace('true', '"true"') 
-tag = str(tag).replace('false', '"false"')
-tag = tag.strip("'<>() ").replace('\'', '\"')
+tag = tag.replace('>', '')
 
 # Tries to read the tag in the json formal and here the errors appear
 data = json.loads(tag)
+print(data[0]['props']['pageProps'].keys())
+
+# If you want to see all the types of the data that we have, please do replace print(project['nft_name']) with print(project.keys())
+# In case you decide that you want to see other kind of data, please replace nft_name with the type of data(key) that you want to see
+for project in data[0]['props']['pageProps']['projects']:
+    print(project['nft_name'])
